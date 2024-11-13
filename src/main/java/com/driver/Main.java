3e59b3467812d0f7419860ed4482cc97c75be063
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2024-11-13 23:41:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
+        
+      public static  class product{
+       
+            public int sum(int x,int y){
+                return x+y;
+                
+            }
+            public int sum(int x, int y, int z) {
+                return x+y+z;
 
-}
+            }
+            public double sum(double x, double y) {
+                return x+y;
+
+            }
+        }
+        public static void main(String args[]){
+            product p=new product();
+
+          System.out.println(p.sum(10,20));
+          System.out.println(p.sum(20,10,30));
+          System.out.println(p.sum(40,50,60));
+        
+
+        }
+    }
+
